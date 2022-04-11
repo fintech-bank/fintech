72c1fe0625fce67c390f916f59d783ef08fc3825
mmockelyn <trainznation@gmail.com>
2022-04-11 17:10:09 +0200

    CMS Core

---
+++ app/Models/Cms/CmsSubCategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Cms;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CmsSubCategory extends Model
+{
+    use HasFactory;
+}
--- database/migrations/2022_04_11_170921_create_cms_sub_categories_table.php
+++ database/migrations/2022_04_11_170921_create_cms_sub_categories_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('cms_categories', function (Blueprint $table) {
+        Schema::create('cms_sub_categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->timestamps();
         });
     }
 
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cms_categories');
+        Schema::dropIfExists('cms_sub_categories');
     }
 };
