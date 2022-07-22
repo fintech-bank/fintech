c6463014b8ff400080763c718d4c875e60dcb38c
Mockelyn Maxime <trainznation@gmail.com>
2022-07-22 14:12:27 +0200

    FX220720221411
    - Correction systeme authentificatop,
    - Correction mouvement carte différé

--- app/Helper/CustomerTransactionHelper.php
+++ app/Helper/CustomerTransactionHelper.php
@@ -75,7 +75,7 @@ class CustomerTransactionHelper
         }
     }
 
-    public static function create($type, $type_transaction, $description, $amount, $wallet, $confirm = true, $designation = null, $confirmed_at = null, $updated_at = null, $card_id = null)
+    public static function create($type, $type_transaction, $description, $amount, $wallet, $confirm = true, $designation = null, $confirmed_at = null, $updated_at = null, $card_id = null, $differed = false)
     {
         if($type == 'debit') {
             CustomerTransaction::create([
@@ -89,6 +89,8 @@ class CustomerTransactionHelper
                 "customer_wallet_id" => $wallet,
                 "updated_at" => $updated_at,
                 "customer_credit_card_id" => $card_id != null ? $card_id : null,
+                "differed" => $differed ? 1 : 0,
+                "differed_at" => $differed ? now()->endOfMonth() : null
             ]);
             $transaction = CustomerTransaction::with('wallet')->latest()->first();
 
